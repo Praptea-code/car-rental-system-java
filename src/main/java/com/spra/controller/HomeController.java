e54f45a4f837ffa6d954070e12e2ecbbdbbd6d80
Praptea-code <bhattarai.prapti00@gmail.com>
2026-04-14 16:56:34 +0545

    fix: remove / from homecontroller

--- src/main/java/com/spra/controller/HomeController.java
+++ src/main/java/com/spra/controller/HomeController.java
@@ -14,7 +14,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(urlPatterns = {"/", "/home"}, asyncSupported = true)
+@WebServlet(urlPatterns = { "/home"}, asyncSupported = true)
 public class HomeController extends HttpServlet {
 
     private final ProductDAO productDAO;
